b3b758225dfd5cd01a422653cdcb9f10a1b6d91e
Soon Lee Edmund Lam <edmund.lam@colorado.edu>
2017-03-03 14:27:32 -0700

    generalized shell output (STDOUT, STDERR) representation

--- src/main/scala/edu/colorado/plv/fixr/bash/Bash.scala
+++ src/main/scala/edu/colorado/plv/fixr/bash/Bash.scala
@@ -6,23 +6,16 @@ package edu.colorado.plv.fixr.bash
 
 import scala.sys.process._
 
-case class BashStdout(output: String) {
-  override def toString: String = s"Success: $output"
-}
-
-case class BashStderr(errMsg: String) {
-  override def toString: String = s"Failure: $errMsg"
-}
-
-case class BashResults(exitCode: Int, stdout: String, stderr: String) {
-
+case class BashResult[O,E](exitCode:Int, stdout: O, stderr: E) {
+  def getExitCode(): Int = exitCode
+  def getStdout(): O = stdout
+  def getStderr(): E = stderr
   override def toString: String = {
-     s"EXITCODE :- $exitCode\nSTDOUT :- $stdout\nSTDERR :- $stderr"
+    s"EXITCODE :- $exitCode\nSTDOUT :- $stdout\nSTDERR :- $stderr"
   }
-
 }
 
-class BashResultBuilder() {
+class BashResultBuilder[O,E](outParser: String => O, errParser: String => E) {
 
   val out  = new StringBuilder
   val err  = new StringBuilder
@@ -32,29 +25,35 @@ class BashResultBuilder() {
   def addOutMsg(o: String) = out.append( o + getSep() )
   def addErrMsg(e: String) = err.append( e + getSep() )
 
-  def formatStdout(): String = out.split(getSep().toCharArray).filter( _.length!=0 ).mkString("\n")
-  def formatStderr(): String = err.split(getSep().toCharArray).filter( _.length!=0 ).mkString("\n")
+  def formatStdout(): O = outParser( out.split(getSep().toCharArray).filter( _.length!=0 ).mkString("\n") )
+  def formatStderr(): E = errParser( err.split(getSep().toCharArray).filter( _.length!=0 ).mkString("\n") )
 
-  def mkResults(exitcode: Int): BashResults = BashResults(exitcode, formatStdout(), formatStderr())
+  def mkResults(exitcode: Int): BashResult[O,E] = BashResult[O,E](exitcode, formatStdout(), formatStderr())
 
 }
 
+class StdResultBuilder extends BashResultBuilder[String,String](identity,identity)
+
 object BashLogger {
 
-  def mkBashLogger(): (ProcessLogger,BashResultBuilder) = {
-    val outcomeBuilder = new BashResultBuilder
+  def mkBashLogger[O,E](outParser: String => O, errParser: String => E): (ProcessLogger,BashResultBuilder[O,E]) = {
+    val resultBuilder = new BashResultBuilder[O,E](outParser,errParser)
 
     val logger = ProcessLogger(
-       (o: String) => outcomeBuilder.addOutMsg( o ),
-       (e: String) => outcomeBuilder.addErrMsg( e ) )
-    return (logger,outcomeBuilder)
+      (o: String) => resultBuilder.addOutMsg( o ),
+      (e: String) => resultBuilder.addErrMsg( e ) )
+    return (logger,resultBuilder)
+  }
+
+  def mkBashLogger(): (ProcessLogger,BashResultBuilder[String,String]) = {
+    mkBashLogger[String,String](identity,identity)
   }
 
 }
 
 object Bash {
 
-  def runCmd(cmd: String): BashResults = {
+  def runCmd(cmd: String): BashResult[String,String] = {
     val (logger,outcome) = BashLogger.mkBashLogger()
     val res = cmd ! logger
     outcome.mkResults(res)
@@ -69,3 +68,6 @@ object Bash {
   }
 
 }
+
+
+
