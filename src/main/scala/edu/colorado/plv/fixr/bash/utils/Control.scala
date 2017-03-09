d382e3c4735a95e0f7dcb4fd375603defe4cc6da
Soon Lee Edmund Lam <edmund.lam@colorado.edu>
2017-03-09 16:36:51 -0700

    added try catch construct

--- src/main/scala/edu/colorado/plv/fixr/bash/utils/Control.scala
+++ src/main/scala/edu/colorado/plv/fixr/bash/utils/Control.scala
@@ -1,7 +1,7 @@
 package edu.colorado.plv.fixr.bash.utils
 
 import com.typesafe.scalalogging.Logger
-import edu.colorado.plv.fixr.bash.{Bash, Fail, Succ}
+import edu.colorado.plv.fixr.bash.{Bash, Cmd, Fail, Succ}
 
 /**
   * Created by edmund on 3/7/17.
@@ -22,7 +22,8 @@ case class repeat(op: Bash) extends Bash {
 
   var condition = (x:TryM[Succ,Fail]) => false
   var waitTime = 1000
-  var tries = 10
+  var tries = -1
+  var ignore = true
 
   def until(c: TryM[Succ,Fail] => Boolean) (implicit bashLogger: Logger): TryM[Succ,Fail] = { condition = c ; this ! }
 
@@ -30,16 +31,44 @@ case class repeat(op: Bash) extends Bash {
 
   def times(number: Int): repeat = { tries = number ; this }
 
+  def ignoreFail(ig: Boolean): repeat = { ignore = ig ; this }
+
   override def !(implicit bashLogger: Logger): TryM[Succ, Fail] = {
     var count = 0
     while (true) {
        val res = for{ p <- op ! } yield p
-       if (condition(res)) return res
+       if (!ignore) {
+         res match {
+           case FailTry(_) => return res
+           case default    =>
+         }
+       }
+       if (condition(res)) {
+         res match {
+           case FailTry(Fail(c, ec, o, e)) => return SuccTry(Succ("<RepeatUntil>", o, e))
+           case default => return res
+         }
+       }
        count += 1
        if (tries > 0 && count > tries) return SuccTry(Succ("<RepeatUntil>", "Exceeded number of tries", ""))
        Thread.sleep(waitTime)
     }
     FailTry(Fail("<RepeatUntil>", 2, "", "This is unreachable."))
+  }
+
+}
+
+case class tryDo(op: Bash) extends Bash {
+
+  var contingencies: Fail => Bash = (f:Fail) => Cmd(s"echo Try failed: ${f.stderr}")
+
+  def catchWith(c: Fail => Bash) (implicit bashLogger: Logger): TryM[Succ,Fail] = { contingencies = c ; this ! }
+
+  override def ! (implicit bashLogger: Logger): TryM[Succ, Fail] = {
+     op ! match {
+       case SuccTry(Succ(c,o,e)) => SuccTry(Succ(c,o,e))
+       case FailTry(fail) => contingencies(fail) !
+     }
   }
 
 }
